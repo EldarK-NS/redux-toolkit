d700092a52cc73cd009f960c554f9d6c45631494
EldarK-NS <eldark.ns@gmail.com>
2021-10-27 13:27:42 +0600

    slice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField";
+import TodoList from "./components/TodoList";
+import { useDispatch } from "react-redux";
+import { addTodo } from "./store/todoSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const [text, setText] = useState("");
+
+  const addTask = () => {
+    dispatch(addTodo({ text }));
+    setText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+      <ul>
+        <TodoList />
+      </ul>
     </div>
   );
 }
